c74387b3e633dcb2cb946757ff1fcb8ab26f693f
Taylor Nix <you@example.com>
2019-09-03 16:42:06 -0500

    initial commit

---
+++ Desktop/HelloWorld.java/HelloWorld.java
@@ -0,0 +1,16 @@
+/**
+ *
+ * @author Taylor Nix
+ */
+
+public class Class1 {
+
+    private void f1() {
+        // v1.0
+    }
+    private void f2() {
+        // v2.0
+    }
+
+
+}
---
+++ Desktop/cs310-hello-world-master/HelloWorld.java
@@ -0,0 +1,23 @@
+/**
+ *
+ * @author Taylor Nix
+ */
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        Class1 = new Class1();
+        Class2 = new Class2();
+        System.out.println("Hello, World!");
+    }
+    private void f1() {
+        // v1.0
+    }
+    private void f2() {
+        // v2.0
+    }
+    private void f3() {
+        // v3.0
+    }
+
+}
